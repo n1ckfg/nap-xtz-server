f1de1aff7679a9613a680cb47e1a942d2ebcf6cb
Nick Fox-Gieg <nick@fox-gieg.com>
2026-09-09 16:10:37 -0400

    shell

--- kiosk-rpi.sh
+++ kiosk-rpi.sh
@@ -1,9 +1,9 @@
 #--incognito --kiosk --start-fullscreen --use-fake-ui-for-media-stream --noerrdialogs --disable-infobars --no-first-run --ozone-platform=wayland
 
 APP_PATH="chromium-browser"
-CMD="--kiosk --noerrdialogs --disable-infobars --use-fake-ui-for-media-stream --no-first-run --ozone-platform=wayland"
+FLAGS="--kiosk --noerrdialogs --disable-infobars --use-fake-ui-for-media-stream --no-first-run --ozone-platform=wayland"
 
 echo $XDG_SESSION_TYPE
 node app.js &
-$APP_PATH $CMD "http://localhost:8080"
+$APP_PATH $FLAGS "http://localhost:8080"
 
