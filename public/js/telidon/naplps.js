f5d0bdb4eb17828bfbba729aa751e04c3682bcca
Nick Fox-Gieg <nick@fox-gieg.com>
2026-09-10 00:28:46 -0400

    Note of proposed naplps encoder fix

--- public/js/telidon/naplps.js
+++ public/js/telidon/naplps.js
@@ -1488,6 +1488,8 @@ class NapEncoder {
 
 		const intX = parseInt(Math.abs(input.x) * this.maxBitVals);
 		const intY = parseInt(Math.abs(input.y) * this.maxBitVals);
+        //const intX = Math.min(this.maxBitVals - 1, Math.round(Math.abs(input.x) * this.maxBitVals));
+        //const intY = Math.min(this.maxBitVals - 1, Math.round(Math.abs(input.y) * this.maxBitVals));
 		if (this.debug) console.log("Converting vector to int: " + intX + ", " + intY);
 
 		let binX = binary(intX, this.bitExponent); 
